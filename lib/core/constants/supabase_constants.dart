e0e9727f828ea164295a8b6ead9b120bb3dfcc3b
Tai Nguyen Van <buiblue123@gmail.com>
2026-03-14 16:19:08 +0700

    fix: change to load env from .env

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -1,13 +1,21 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class SupabaseConstants {
   SupabaseConstants._();
 
-  static const String supabaseUrl = String.fromEnvironment(
-    'SUPABASE_URL',
-    defaultValue: 'YOUR_SUPABASE_URL',
-  );
+  // Load from --dart-define at compile time
+  // static const String supabaseUrl = String.fromEnvironment(
+  //   'SUPABASE_URL',
+  //   defaultValue: 'YOUR_SUPABASE_URL',
+  // );
 
-  static const String supabaseAnonKey = String.fromEnvironment(
-    'SUPABASE_ANON_KEY',
-    defaultValue: 'YOUR_SUPABASE_ANON_KEY',
-  );
+  // static const String supabaseAnonKey = String.fromEnvironment(
+  //   'SUPABASE_ANON_KEY',
+  //   defaultValue: 'YOUR_SUPABASE_ANON_KEY',
+  // );
+
+  static String get supabaseUrl =>
+      dotenv.env['SUPABASE_URL'] ?? 'YOUR_SUPABASE_URL';
+  static String get supabaseAnonKey =>
+      dotenv.env['SUPABASE_ANON_KEY'] ?? 'YOUR_SUPABASE_ANON_KEY';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,12 @@ import 'viewmodels/settings/settings_viewmodel.dart';
 import 'views/auth/reset_password_screen.dart';
 import 'views/splash/splash_screen.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
 
   await Supabase.initialize(
     url: SupabaseConstants.supabaseUrl,
